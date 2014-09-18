f3c4dfddaa938f240a0cb36c0287f08d6a8e3701
nickmeccia <nickmeccia@gmail.com>
2014-09-18 12:22:41 -0500

    Fixed the background scrollability by myself! Ha! Well...Ginny helped.

--- public/javascripts/scroll.js
+++ public/javascripts/scroll.js
@@ -4,8 +4,10 @@ $('document').ready(function() {
   var bg = $('.background')
   var win = $(window)
 
+  bg.css({'background-position': 'center -120px'})
+
   win.scroll(function() {
     var top = win.scrollTop()
-    bg.css({'background-position': '0px ' + -1 * 0.1*top + 'px'})
+    bg.css({'background-position': 'center ' + ((-1 * 0.1*top) - 120) + 'px'})
   })
 })
